08da3cf8ad226680dcfce5230342dec9bf71db4d
Khushi Singhania <109690883+khushiSinghania11@users.noreply.github.com>
2023-07-19 01:14:20 +0530

    Create upload_test.php

---
+++ upload_test.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> Upload Test! </title>
+</head>
+<body>
+    <form action="process.php" method="post">
+        Test Name:
+        <input type="text"  name="test_name"><br>
+        Question1:</label>
+        <input type="text" name="question1"><br>
+
+        Option 1:
+        <input type="text" name="option1_1"><br>
+
+        Option 2:
+        <input type="text"  name="option1_2"><br>
+
+        Option 3:
+        <input type="text" name="option1_3"><br>
+
+        Option 4:
+        <input type="text" name="option1_4"><br>
+
+        Correct option:<br>
+        Option 1
+        <input type="radio"  name="correct_option1" value="1"><br>
+        
+        Option 2
+        <input type="radio"  name="correct_option1" value="2"><br>
+        
+        Option 3
+        <input type="radio"  name="correct_option1" value="3"><br>
+        
+        Option 4
+        <input type="radio"  name="correct_option1" value="4"><br>
+        
+        Question2:
+        <input type="text"  name="question2"><br>
+
+        Option 1:
+        <input type="text" name="option2_1"><br>
+
+        Option 2:
+        <input type="text"  name="option2_2"><br>
+
+        Option 3:
+        <input type="text" name="option2_3"><br>
+
+        Option 4:
+        <input type="text" name="option2_4"><br>
+
+        Correct option:<br>
+        Option 1
+        <input type="radio"  name="correct_option2" value="1"><br>
+        
+        Option 2
+        <input type="radio"  name="correct_option2" value="2"><br>
+        
+        Option 3
+        <input type="radio"  name="correct_option2" value="3"><br>
+        
+        Option 4
+        <input type="radio"  name="correct_option2" value="4"><br>
+        
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
