6da9d925e2d57994efad6d5205826a3e3eba7a33
Khushi Singhania <109690883+khushiSinghania11@users.noreply.github.com>
2023-07-19 01:03:15 +0530

    Create db_configure.php

---
+++ db_configure.php
@@ -0,0 +1,17 @@
+<?php
+
+$sname= "localhost";
+
+$uname= "root";
+
+$password = "";
+
+$db_name = "login_page";
+
+$conn = mysqli_connect($sname, $uname, $password, $db_name);
+
+if (!$conn) {
+
+    echo "Connection failed!";
+}
+?>
